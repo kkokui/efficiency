e8e7fdc1e081eeb85f93c1e77507d9b9ffaa425a
kkokui <18s2014b@student.gs.chiba-u.jp>
2023-04-18 11:12:27 +0900

    Add an input file for the information of the volume.

---
+++ make_default_volume_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "example=0">> volume_info.txt
+echo "module=">> volume_info.txt
+echo "zone=">> volume_info.txt
+echo "plMin=">> volume_info.txt
+echo "plMax=">> volume_info.txt
+echo "reco=">> volume_info.txt
+echo "Xcenter=">> volume_info.txt
+echo "Ycenter=">> volume_info.txt
