1aaf5179cb5f387bd22dd407910547b98547a729
kkokui <18s2014b@student.gs.chiba-u.jp>
2023-05-15 21:11:00 +0900

    Add argument of plate.

--- efficiency.cpp
+++ efficiency.cpp
@@ -5,114 +5,46 @@
 #include <TCanvas.h>
 #include <TEfficiency.h>
 #include <TH1.h>
-std::string module;
-int zone, plMin, plMax, reco;
-double Xcenter, Ycenter;
-int read_volume_info()
-{
-	std::ifstream file;
-	file.open("volume_info.txt");	
-	if(file.fail())
-	{
-		printf("Failed to open volume_info.txt\n");
-		return 0;
-	}
-	std::string line;
-	int count=0;
-	while (std::getline(file, line))
-	{
-		std::vector<std::string> elems;
-		std::stringstream ss(line);
-		std::string item;
-		while(std::getline(ss, item, '='))
-		{
-			if(!item.empty())
-				elems.push_back(item);
-		}
-		if("module"==elems.at(0))
-		{
-			module = elems.at(1);
-			count++;
-		}
-		if ("zone" == elems.at(0))
-		{
-			zone = std::stoi(elems.at(1));
-			count++;
-		}
-		if ("plMin" == elems.at(0))
-		{
-			plMin = std::stoi(elems.at(1));
-			count++;
-		}
-		if ("plMax" == elems.at(0))
-		{
-			plMax = std::stoi(elems.at(1));
-			count++;
-		}
-		if ("reco" == elems.at(0))
-		{
-			reco = std::stoi(elems.at(1));
-			count++;
-		}
-		if ("Xcenter" == elems.at(0))
-		{
-			Xcenter = std::stod(elems.at(1));
-			count++;
-		}
-		if ("Ycenter" == elems.at(0))
-		{
-			Ycenter = std::stod(elems.at(1));
-			count++;
-		}
-		
-	}
-	// printf("%s,%d,%d,%d,%d,%f,%f\n",module.c_str(),zone,plMin,plMax,reco,Xcenter,Ycenter);
-	if (7 == count)
-	{
-		printf("volume_info.txt read successfully\n");
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
 
 int main(int argc , char *argv[]){
-	if(argc<=2){
-		printf("Usage : ./efficiency linked_tracks.root title \"nseg>=5\"\n");
-		return 0;
+	if(argc<=4){
+		printf("Usage : ./efficiency linked_tracks.root title plMin plMax\n");
+		return 1;
 	}
-	if(0==read_volume_info())
-	{
-		printf("Could not read volume_info.txt\n");
-		return 0;
-	}
-	TString linked_tracks = argv[1];
+	
+	int plMin, plMax;
+	TString filename_linked_tracks = argv[1];
 	TString title = argv[2];
-	TString cut = argv[3];
+	sscanf(argv[3], "%d", &plMin);
+	sscanf(argv[4], "%d", &plMax);
+	TString cut = "nseg>=5";
 	EdbDataProc *dproc = new EdbDataProc;
 	EdbPVRec *pvr = new EdbPVRec;
-	// dproc->ReadTracksTree(*pvr, "linked_tracks.root",Form("nseg>=5&& abs(t.eTX + 0.01) < 0.01 && abs(t.eTY-0.004) < 0.01"));
- 	dproc->ReadTracksTree(*pvr, linked_tracks, cut);
+ 	dproc->ReadTracksTree(*pvr, filename_linked_tracks, cut);
 	TEfficiency *pEff_angle =0;
 	TEfficiency *pEff_plate =0;
 	
 	int ntrk = pvr->Ntracks();
-	double x1, y1, z1, x2, y2, z2, angle;
 	double bins[] = {0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2,0.25,0.3,0.35,0.4,0.45,0.5};
 	int nbins = 26;
 	
-	// TH1D *h_angle_total = new TH1D("total",";angle;",10,0,0.5);
-	// TH1D *h_angle_passed = new TH1D("passed",";angle;",10,0,0.5);
 	TH1D *h_angle_total = new TH1D("hist_angle_total", title+";tan#theta;", nbins, bins);
 	TH1D *h_angle_passed = new TH1D("hist_angle_passed", title+";tan#theta;", nbins, bins);
 	TH1D *h_plate_total = new TH1D("hist_plate_total", title+";plate;", plMax-plMin, plMin,plMax);
 	TH1D *h_plate_passed = new TH1D("hist_plate_passed", title + ";plate;", plMax - plMin, plMin, plMax);
-	TFile ntfout(Form("nt_not_passed_%s.root",title.Data()), "recreate");
-	double x, y;
-	TNtuple *nt = new TNtuple("nt","title","trackID:x:y:angle:pl:nseg");
 
+	TFile treefout(Form("tree_not_passed_%s.root",title.Data()), "recreate"); //to check the distribution of passed segments
+	TNtuple *tree = new TNtuple("tree","title","trackID:x:y:angle:pl:nseg");
+	int trackID,pl,nseg;
+	double x, y, angle;
+	tree->Branch("trackID",&trackID);
+	tree->Branch("x",&x);
+	tree->Branch("y",&y);
+	tree->Branch("angle",&angle);
+	tree->Branch("pl",&pl);
+	tree->Branch("nseg",&nseg);
+
+	double x1, y1, z1, x2, y2, z2;
 	for(int itrk=0; itrk<ntrk; itrk++){
 		EdbTrackP *t = pvr->GetTrack(itrk);
 		int nseg = t->N();
@@ -151,14 +83,14 @@ int main(int argc , char *argv[]){
 				}
 				else
 				{
-					nt->Fill(t->ID(),(x1+x2)/2,(y1+y2)/2,angle,iplate,nseg);
+					tree->Fill(t->ID(),(x1+x2)/2,(y1+y2)/2,angle,iplate,nseg);
 				}
 			}
 			
 		}
 		
 	}
-	nt->Write();
+	tree->Write();
 	TCanvas *c = new TCanvas();
 	c->Print(Form("hist_efficiency_%s.pdf[", title.Data()));
 	c->SetLogy(1);
@@ -181,7 +113,7 @@ int main(int argc , char *argv[]){
 	pEff_plate->Write();
 	fout.Close();
 
-	FILE *ftxt = fopen("efficiency.txt","w");
+	FILE *ftxt = fopen("efficiency.txt","w"); //Make txt file for smearing parameters.
 	for(int i=1;i<=nbins;i++)
 	{
 		fprintf(ftxt, "%.3f, ",(bins[i]+bins[i-1])/2);
@@ -193,5 +125,5 @@ int main(int argc , char *argv[]){
 	}
 	fprintf(ftxt,"\n");
 	fclose(ftxt);
-	return 1;
+	return 0;
 }
